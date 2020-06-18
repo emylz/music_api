c919c2f6936a2fc53a50ab9e36e9129706c00d43
emylz <43544835+emylz@users.noreply.github.com>
2020-06-18 16:53:27 +0200

    Update artists.py

--- artists.py
+++ artists.py
@@ -33,7 +33,7 @@ def get_all_artists():
     #Output where results will be stored
     output = []
 
-    #Find all artists and get their attributs : type, id and tags
+    #Find all artists and get their attributs : type, ID and tags
     for a in artists.find():
         output.append({'type':a['type'], 'id':a['_id'], 'tags':a['tags']})
 
