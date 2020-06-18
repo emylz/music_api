d3cf9089e9335ee0ec8c77d7289f9ec49deee016
emylz <43544835+emylz@users.noreply.github.com>
2020-06-18 16:53:02 +0200

    Update albums.py

--- albums.py
+++ albums.py
@@ -33,7 +33,7 @@ def get_all_albums():
     #Output where results will be stored
     output = []
 
-    #Find all albums and get their attributs : type, id and tags
+    #Find all albums and get their attributs : type, ID and tags
     for a in albums.find():
         output.append({'type':a['type'], 'id':a['_id'], 'tags':a['tags']})
     
